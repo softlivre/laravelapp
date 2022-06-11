7f23fcaef4a5e1b1417266a979d7ce3c07b82ea3
softlivre <softlivre1@gmail.com>
2022-06-11 16:05:32 -0300

    now we can create our posts into the DB!

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -32,7 +32,7 @@ class PostsController extends Controller
      */
     public function create()
     {
-        //
+        return view('posts.create');
     }
 
     /**
@@ -43,7 +43,18 @@ class PostsController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $this->validate ($request, [
+            'title' => 'required',
+            'body' => 'required'
+        ]);
+        // Create Post
+        $post = new Post;
+        $post->title = $request->input('title');
+        $post->body = $request->input('body');
+        $post->save();
+
+        return redirect('/posts')->with('success', 'Post Created');
+         
     }
 
     /**
--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -24,6 +24,10 @@
             <a class="nav-link" href="/posts">Blog</a>
           </li>
         </ul>
+
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="/posts/create">Create Post</a></li>
+        </ul>
         
       </div>
     </div>
