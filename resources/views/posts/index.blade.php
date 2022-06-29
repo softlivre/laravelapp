d4e08376f28924bc9240df14eadd197010ead0a2
softlivre <softlivre1@gmail.com>
2022-06-28 21:20:09 -0300

    now we have implemented image manipulation on the posts, again

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -12,9 +12,7 @@
                 <div class="col-md-8 col-sm-8">
                     <h3><a href="/posts/{{$post->id}}">{{$post->title}}</a></h3>
                     <small>Written on {{$post->created_at}} by {{$post->user->name}}</small>                    
-                </div>
-
-               
+                </div>               
             </div>
             <br>
         @endforeach
